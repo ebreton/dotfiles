af6833e40de7e6c1621f52981ab2603716b104d5
ebreton <email>
2018-04-02 20:35:56 +0200

    exclude macos file in default setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,10 @@ function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
+		--exclude ".macos" \
 		--exclude "bootstrap.sh" \
 		--exclude "brew.sh" \
+		--exclude ".macos_aliases" \
 		--exclude "init" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
